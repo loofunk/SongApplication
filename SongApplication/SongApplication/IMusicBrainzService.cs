298ca7130224e4591498adac6ae56b8f9865b484
Louis <louis.dasouki@carfinance247.co.uk>
2020-10-30 09:11:11 +0000

    Project Scaffold setup
    Set up basic scaffold of project and unit test project

---
+++ SongApplication/SongApplication/IMusicBrainzService.cs
@@ -0,0 +1,6 @@
+﻿namespace SongApplication
+{
+    public interface IMusicBrainzService
+    {
+    }
+}
---
+++ SongApplication/SongApplication/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SongApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
